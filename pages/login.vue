cabdeeff1e4ab65104b1cb3e61aaeacd5b36e922
Naoko3in4 <okashimochi@gimail.com>
2020-05-17 13:36:46 +0900

    add cookie

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>ログインページ</h2>
+    <p>
+      <button>ログイン</button>
+      <nuxt-link :to="{ name: '/' }">
+        トップページに戻る
+      </nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
