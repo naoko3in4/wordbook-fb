10ef946db1785773404fd47e9679b1796f747b4a
Naoko3in4 <okashimochi@gimail.com>
2020-05-17 12:40:59 +0900

    header in posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,40 +1,47 @@
 <template>
-  <div class="container pt-20">
-    <h2 class="list-title">登録単語一覧</h2>
-    <div
-      class="all_word_list__wrapper max-w-xs my-0 mx-auto mb-6 p-5 pb-8 rounded-lg overflow-scroll bg-white"
-    >
+  <div>
+    <the-header />
+    <div class="container pt-20">
+      <h2 class="list-title">登録単語一覧</h2>
       <div
-        v-for="content in contents"
-        :key="content.sys.id"
-        class="all_word-list flex justify-between"
+        class="all_word_list__wrapper max-w-xs my-0 mx-auto mb-6 p-5 pb-8 rounded-lg overflow-scroll bg-white"
       >
-        <div class="leading-8 whitespace-no-wrap overflow-scroll">
-          <span class="text-blue-700 font-bold">{{ content.fields.word }}</span
-          ><span class="text-sm">:{{ content.fields.meaning }}</span>
-        </div>
         <div
-          class="edit-btn w-10 leading-8 rounded-md text-center text-gray-500 border border-gray-400"
-          @click="goToEditPage(content.sys.id)"
+          v-for="content in contents"
+          :key="content.sys.id"
+          class="all_word-list flex justify-between"
         >
-          編集
+          <div class="leading-8 whitespace-no-wrap overflow-scroll">
+            <span class="text-blue-700 font-bold">{{
+              content.fields.word
+            }}</span
+            ><span class="text-sm">:{{ content.fields.meaning }}</span>
+          </div>
+          <div
+            class="edit-btn w-10 leading-8 rounded-md text-center text-gray-500 border border-gray-400"
+            @click="goToEditPage(content.sys.id)"
+          >
+            編集
+          </div>
         </div>
       </div>
-    </div>
 
-    <nuxt-link :to="{ name: 'index' }">
-      <app-buttton color="grey" text="戻る" />
-    </nuxt-link>
+      <nuxt-link :to="{ name: 'index' }">
+        <app-buttton color="grey" text="戻る" />
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
 // import contentfulClient from '@/plugins/contentful'
 import AppButtton from '@/components/AppButton.vue'
+import TheHeader from '@/components/TheHeader.vue'
 
 export default {
   components: {
-    AppButtton
+    AppButtton,
+    TheHeader
   }
   // asyncData({ env }) {
   //   return contentfulClient
