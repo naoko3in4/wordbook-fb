d99433f226d60a90a7fff0cc7a2a698e807943d8
Naoko3in4 <okashimochi@gimail.com>
2020-05-17 14:10:23 +0900

    make login page

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>ログインページ</h2>
     <p>
-      <button>ログイン</button>
+      <button @click="login()">ログイン</button>
       <nuxt-link :to="{ name: '/' }">
         トップページに戻る
       </nuxt-link>
@@ -11,7 +11,17 @@
 </template>
 
 <script>
-export default {}
+import Cookies from 'universal-cookie'
+
+export default {
+  methods: {
+    login() {
+      const cookies = new Cookies()
+      cookies.set('credintial', 'true', { maxAge: 90 })
+      this.$router.push('/')
+    }
+  }
+}
 </script>
 
 <style></style>
